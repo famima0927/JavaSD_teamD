dea83b56c80ad9578c83bd497e7490fb3e3d5bfe
h_komoriya <h_komoriya@LAPTOP-6S6E49BV>
2025-06-10 11:21:42 +0900

    DAOの仮ページを作った(中身なし)

---
+++ src/dao/SchoolDao.java
@@ -0,0 +1,5 @@
+package dao;
+
+public class SchoolDao {
+	//StudentDaoに学校の情報などを継承するためのdao
+}
---
+++ src/dao/SubjectDao.java
@@ -0,0 +1,34 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.security.auth.Subject;
+
+public class SubjectDao extends Dao {
+
+
+	public String get(String cd,school school) throws Exception{
+
+
+		return null;
+	}
+
+
+	public List<Subject> filter() throws Exception{
+	    List<Subject> list = new ArrayList<>();
+
+		return list;
+	}
+
+
+	public boolean save(subject subject){
+
+	}
+
+
+	public boolean delete(subject suvject) throws Exception{
+
+	}
+}
+
