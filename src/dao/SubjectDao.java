dba97fca2eb7e9e35ba714feeb544ddfeae9bf6f
h_komoriya <h_komoriya@LAPTOP-6S6E49BV>
2025-06-11 11:59:35 +0900

    aaa

--- src/dao/SubjectDao.java
+++ src/dao/SubjectDao.java
@@ -12,16 +12,15 @@ import bean.Subject;
 public class SubjectDao extends Dao {
 
 
-	//学生の一人の成績を検索して
+
 	public Subject get(String cd,School school) throws Exception{
 		Subject subject = new Subject();
-
+		
 		//引数をもとにデータベースから科目(subject)を返す関数
 
 		return subject;
 	}
 
-	//schoolをもとに科目の一覧を返す
 	public List<Subject> filter(School school) throws Exception{
 	    List<Subject> list = new ArrayList<>();
 
@@ -29,7 +28,6 @@ public class SubjectDao extends Dao {
 	}
 
 
-	//科目(subject)の保存を行う
 	public boolean save(Subject subject) throws Exception{
 		boolean isSave = false;
 
