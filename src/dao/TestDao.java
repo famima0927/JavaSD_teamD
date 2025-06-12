9758ccb98927dd566a4bdfb3f7727538ae7a0f8d
h_komoriya <h_komoriya@LAPTOP-6S6E49BV>
2025-06-12 10:22:51 +0900

    daoです

--- src/dao/TestDao.java
+++ src/dao/TestDao.java
@@ -1,7 +1,9 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,51 +14,184 @@ import bean.Test;
 
 public class TestDao extends Dao {
 
-	public String baseSql="";
+    /**
+     * [private] PKを指定してTestを1件取得する。privateなsave/deleteから利用される。
+     */
+    private Test get(Student student, Subject subject, int no, Connection connection) throws Exception {
+        Test test = null;
+        String sql = "SELECT * FROM TEST WHERE STUDENT_NO = ? AND SUBJECT_CD = ? AND SCHOOL_CD = ? AND NO = ?";
 
-	public Test get(Student student,Subject subject ,School school ,int no){
-		Test test = new Test();
+        try (PreparedStatement st = connection.prepareStatement(sql)) {
+            st.setString(1, student.getNo());
+            st.setString(2, subject.getCd());
+            st.setString(3, student.getSchool().getCd());
+            st.setInt(4, no);
+            try (ResultSet rs = st.executeQuery()) {
+                if (rs.next()) {
+                    test = new Test();
+                    test.setStudent(student);
+                    test.setSubject(subject);
+                    test.setSchool(student.getSchool());
+                    test.setNo(no);
+                    test.setPoint(rs.getInt("POINT"));
+                    test.setClassNum(rs.getString("CLASS_NUM"));
+                }
+            }
+        }
+        return test;
+    }
 
-		return test;
-	}
+    /**
+     * [参照] TestのPKを指定して1件取得する
+     */
+    public Test get(Student student, Subject subject, School school, int no) throws Exception {
+        try (Connection con = getConnection()) {
+            // privateのgetメソッドを呼び出す
+            return get(student, subject, no, con);
+        }
+    }
 
-	public List<Test> postFilter(ResultSet rSet,School school){
-		List<Test> list = new ArrayList<Test>();
+    /**
+     * [private] ResultSetをTestリストに変換するヘルパーメソッド
+     */
+    private List<Test> postFilter(ResultSet rSet, School school) throws Exception {
+        List<Test> list = new ArrayList<>();
+        StudentDao studentDao = new StudentDao();
+        SubjectDao subjectDao = new SubjectDao();
 
-		return list;
-	}
+        try {
+            while (rSet.next()) {
+                Test test = new Test();
+                test.setSchool(school);
+                // 学生、科目は各DAOから取得してセット
+                test.setStudent(studentDao.get(rSet.getString("student_no")));
+                test.setSubject(subjectDao.get(rSet.getString("subject_cd"), school));
+                test.setNo(rSet.getInt("no"));
+                test.setPoint(rSet.getInt("point"));
+                test.setClassNum(rSet.getString("class_num"));
+                list.add(test);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        }
+        return list;
+    }
 
-	public List<Test> Filter(int entYear ,String classNum ,Subject subject ,int num ,School school){
-		List<Test> list = new ArrayList<Test>();
+    /**
+     * [検索] 学生の所属情報と科目、回数を指定して成績データを検索する
+     */
+    public List<Test> filter(int entYear, String classNum, Subject subject, int no, School school) throws Exception {
+        List<Test> list = new ArrayList<>();
+        String sql = "SELECT t.* FROM TEST t JOIN STUDENT s ON t.STUDENT_NO = s.NO " +
+                     "WHERE s.ENT_YEAR = ? AND s.CLASS_NUM = ? AND t.SUBJECT_CD = ? AND t.NO = ? AND t.SCHOOL_CD = ? " +
+                     "ORDER BY t.STUDENT_NO";
 
-		return list;
-	}
+        try (Connection con = getConnection();
+             PreparedStatement st = con.prepareStatement(sql)) {
 
-	public boolean save(List<Test> list){
-		boolean isSave = false;
+            st.setInt(1, entYear);
+            st.setString(2, classNum);
+            st.setString(3, subject.getCd());
+            st.setInt(4, no);
+            st.setString(5, school.getCd());
 
-		return isSave;
+            try (ResultSet rs = st.executeQuery()) {
+                list = postFilter(rs, school);
+            }
+        }
+        return list;
+    }
 
-	}
+    /**
+     * [登録/更新] 複数の成績データをまとめて保存する（トランザクション処理）
+     */
+    public boolean save(List<Test> list) throws Exception {
+        try (Connection con = getConnection()) {
+            // トランザクションを開始
+            con.setAutoCommit(false);
+            try {
+                for (Test test : list) {
+                    save(test, con);
+                }
+                con.commit(); // 全て成功したらコミット
+                return true;
+            } catch (Exception e) {
+                con.rollback(); // エラーが発生したらロールバック
+                e.printStackTrace();
+                return false;
+            }
+        }
+    }
 
-	private boolean save(Test test ,Connection connection){
-		boolean isSave = false;
+    /**
+     * [private] 1件の成績データを保存するヘルパーメソッド
+     */
+    private boolean save(Test test, Connection connection) throws Exception {
+        // 既存データをチェック
+        Test old = get(test.getStudent(), test.getSubject(), test.getNo(), connection);
+        int count = 0;
 
-		return isSave;
+        if (old == null) {
+            // INSERT
+            String sql = "INSERT INTO TEST (STUDENT_NO, SUBJECT_CD, SCHOOL_CD, NO, POINT, CLASS_NUM) VALUES (?, ?, ?, ?, ?, ?)";
+            try (PreparedStatement st = connection.prepareStatement(sql)) {
+                st.setString(1, test.getStudent().getNo());
+                st.setString(2, test.getSubject().getCd());
+                st.setString(3, test.getSchool().getCd());
+                st.setInt(4, test.getNo());
+                st.setInt(5, test.getPoint());
+                st.setString(6, test.getStudent().getClassNum());
+                count = st.executeUpdate();
+            }
+        } else {
+            // UPDATE
+            String sql = "UPDATE TEST SET POINT = ? WHERE STUDENT_NO = ? AND SUBJECT_CD = ? AND SCHOOL_CD = ? AND NO = ?";
+            try (PreparedStatement st = connection.prepareStatement(sql)) {
+                st.setInt(1, test.getPoint());
+                st.setString(2, test.getStudent().getNo());
+                st.setString(3, test.getSubject().getCd());
+                st.setString(4, test.getSchool().getCd());
+                st.setInt(5, test.getNo());
+                count = st.executeUpdate();
+            }
+        }
+        return count > 0;
+    }
 
-	}
+    /**
+     * [削除] 複数の成績データをまとめて削除する（トランザクション処理）
+     */
+    public boolean delete(List<Test> list) throws Exception {
+        try (Connection con = getConnection()) {
+            con.setAutoCommit(false);
+            try {
+                for (Test test : list) {
+                    delete(test, con);
+                }
+                con.commit();
+                return true;
+            } catch (Exception e) {
+                con.rollback();
+                e.printStackTrace();
+                return false;
+            }
+        }
+    }
 
-	public boolean Delete(List<Test> list){
-		boolean isDelete = false;
-
-		return isDelete;
-
-	}
-
-	private boolean Delete(Test test ,Connection connection){
-		boolean isDelete = false;
-
-		return isDelete;
-
-	}
+    /**
+     * [private] 1件の成績データを削除するヘルパーメソッド
+     */
+    private boolean delete(Test test, Connection connection) throws Exception {
+        String sql = "DELETE FROM TEST WHERE STUDENT_NO = ? AND SUBJECT_CD = ? AND SCHOOL_CD = ? AND NO = ?";
+        int count = 0;
+        try (PreparedStatement st = connection.prepareStatement(sql)) {
+            st.setString(1, test.getStudent().getNo());
+            st.setString(2, test.getSubject().getCd());
+            st.setString(3, test.getSchool().getCd());
+            st.setInt(4, test.getNo());
+            count = st.executeUpdate();
+        }
+        return count > 0;
+    }
 }
--- src/dao/TestListSubjectDao.java
+++ src/dao/TestListSubjectDao.java
@@ -1,24 +1,99 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import bean.School;
 import bean.Subject;
 import bean.TestListSubject;
 
 public class TestListSubjectDao extends Dao {
-	public String baseSql = "";
-	private List<TestListSubject> postFilter(ResultSet rSet)throws Exception{
-		List<TestListSubject> list = new ArrayList<TestListSubject>();
 
-		return list;
-	}
+    /**
+     * [private] ResultSetをTestListSubjectのリストに変換するヘルパーメソッド。
+     * 複数の成績レコードを学生一人に集約する処理も行う。
+     * @param rSet: ResultSet SQLの実行結果
+     * @return List<TestListSubject> 科目別の成績リスト
+     * @throws Exception
+     */
+    private List<TestListSubject> postFilter(ResultSet rSet) throws Exception {
+        // 学生番号をキーにTestListSubjectオブジェクトを管理するマップ
+        Map<String, TestListSubject> map = new HashMap<>();
+        // 最終的に返すリスト
+        List<TestListSubject> list = new ArrayList<>();
 
-	public List<TestListSubject> filter(int entYear,String classNum ,Subject subject,School school)throws Exception{
-		List<TestListSubject> list = new ArrayList<TestListSubject>();
+        try {
+            while (rSet.next()) {
+                String studentNo = rSet.getString("student_no");
+                TestListSubject tls = map.get(studentNo);
 
-		return list;
-	}
+                // マップにまだ学生が存在しない場合、新しいオブジェクトを作成してマップとリストに追加
+                if (tls == null) {
+                    tls = new TestListSubject();
+                    tls.setEntYear(rSet.getInt("ent_year"));
+                    tls.setStudentNo(studentNo);
+                    tls.setStudentName(rSet.getString("student_name"));
+                    tls.setClassNum(rSet.getString("class_num"));
+                    map.put(studentNo, tls);
+                    list.add(tls);
+                }
+
+                // 成績（点数）をセットする
+                // rSet.getObject()でNULLをチェック
+                if (rSet.getObject("point") != null) {
+                    tls.putPoint(rSet.getInt("test_no"), rSet.getInt("point"));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        }
+        return list;
+    }
+
+    /**
+     * [検索] 学生の所属情報と科目を指定して、クラスの成績一覧を取得する
+     * @param entYear: int 入学年度
+     * @param classNum: String クラス番号
+     * @param subject: Subject 科目オブジェクト
+     * @param school: School 学校オブジェクト
+     * @return List<TestListSubject>
+     * @throws Exception
+     */
+    public List<TestListSubject> filter(int entYear, String classNum, Subject subject, School school) throws Exception {
+        List<TestListSubject> list = new ArrayList<>();
+
+        // STUDENTテーブルを主軸にTESTテーブルをLEFT JOINする
+        String sql = "SELECT s.ENT_YEAR, s.NO AS student_no, s.NAME AS student_name, s.CLASS_NUM, t.NO AS test_no, t.POINT " +
+                     "FROM STUDENT s " +
+                     "LEFT JOIN TEST t ON s.NO = t.STUDENT_NO AND t.SUBJECT_CD = ? AND t.SCHOOL_CD = ? " +
+                     "WHERE s.ENT_YEAR = ? AND s.CLASS_NUM = ? AND s.SCHOOL_CD = ? " +
+                     "ORDER BY s.NO";
+
+        try (Connection con = getConnection();
+             PreparedStatement st = con.prepareStatement(sql)) {
+
+            // パラメータをセット
+            st.setString(1, subject.getCd());
+            st.setString(2, school.getCd());
+            st.setInt(3, entYear);
+            st.setString(4, classNum);
+            st.setString(5, school.getCd());
+
+            try (ResultSet rs = st.executeQuery()) {
+                list = postFilter(rs);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+        return list;
+    }
 }
