ba6956d432e91e3b3919e3b8d4a87aa06bdf935a
k_sugimoto <k_sugimoto@LAPTOP-P1NTU61R>
2025-06-13 15:04:50 +0900

    s

--- src/servlet/SubjectEditServlet.java
+++ src/servlet/SubjectEditServlet.java
@@ -10,17 +10,24 @@ import dao.SubjectDao;
 import tool.CommonServlet;
 @WebServlet("/SubjectEditServlet")
 public class SubjectEditServlet extends CommonServlet  {
-@Override
-    protected void get(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+	protected void get(HttpServletRequest req, HttpServletResponse resp) throws Exception {
 		// --- 情報の取得 ---
+	try {
+		 String cd = req.getParameter("cd");
 
- 	String cd = req.getParameter("cd");
- 	SchoolDao schD= new SchoolDao();
- 	SubjectDao subD= new SubjectDao();
- 	School sch=schD.get("oom");//学校ID入手
- 	req.setAttribute("subject", subD.get(cd,sch));
- 	req.getRequestDispatcher("Subject/SBJM004.jsp").forward(req, resp);
+		    SchoolDao schD = new SchoolDao();
+		    SubjectDao subD = new SubjectDao();
+		    School sch = schD.get("oom"); // 学校IDを固定で取得
+
+		    req.setAttribute("subject", subD.get(cd, sch));
+
+		    req.getRequestDispatcher("/Subject/SBJM004.jsp").forward(req, resp);
+
+	 } catch (Exception e) {
+		   //エラー時
+			e.printStackTrace();}
 }
+
 
 @Override
 protected void post(HttpServletRequest req, HttpServletResponse resp) throws Exception {
